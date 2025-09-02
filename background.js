66541352d013430c08dd03a7608de349aa5af3d2
Doomx003 <kakaretsoletsika@gmail.com>
2025-09-02 23:35:30 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,19 +1,28 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Incognito Data Eraser installed.");
+const incognitoTabs = new Set();
+
+chrome.tabs.onCreated.addListener((tab) => {
+  if (tab.incognito) {
+    incognitoTabs.add(tab.id);
+  }
 });
 
-chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-  if (removeInfo.isIncognito) {
+chrome.tabs.onRemoved.addListener((tabId) => {
+  if (incognitoTabs.has(tabId)) {
+    incognitoTabs.delete(tabId);
     chrome.browsingData.remove({
-      "since": 0
+      since: 0
     }, {
-      "cache": true,
-      "cookies": true,
-      "history": true,
-      "localStorage": true,
-      "sessions": true
+      cache: true,
+      cookies: true,
+      history: true,
+      localStorage: true,
+      sessions: true
     }, () => {
       console.log("Incognito data erased.");
     });
   }
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Incognito Data Eraser installed.");
 });
